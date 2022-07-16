d1efe8e3d94ba6a1071253141ee938520fa2e98e
Hao Wu <mark14@mail.ustc.edu.cn>
2022-07-16 14:48:00 +0800

    fix mem alloc bug

--- src/CaSync/horovod/mxnet/gradcomp/memory/powersgd.py
+++ src/CaSync/horovod/mxnet/gradcomp/memory/powersgd.py
@@ -11,11 +11,13 @@ class PowersgdMemory(Memory):
         self._resd_gpus = dict()
         self._powersgd_M_gpus = dict()
 
-    def initialize(self, index, grad, rank):
+    def initialize(self, index, grad, low_rank):
+        if index in self._p_memory:
+            return
         # view grad as 2-D matrix(M) with square shape
         interval = math.ceil(math.sqrt(grad.size))
-        self._p_memory[index] = mx.nd.zeros(shape=(interval, rank), dtype=grad.dtype, ctx=grad.context)
-        self._q_memory[index] = mx.nd.normal(shape=(interval, rank), dtype=grad.dtype, ctx=grad.context)
+        self._p_memory[index] = mx.nd.zeros(shape=(interval, low_rank), dtype=grad.dtype, ctx=grad.context)
+        self._q_memory[index] = mx.nd.normal(shape=(interval, low_rank), dtype=grad.dtype, ctx=grad.context)
         self._resd_gpus[index] = mx.nd.zeros(shape=grad.size, dtype=grad.dtype, ctx=grad.context)
         self._powersgd_M_gpus[index] = mx.nd.zeros(shape=(interval, interval), dtype=grad.dtype, ctx=grad.context)
     
